397157ace8f40682a0ba5fb6852f1ec6386a5a63
solremifa <haha95578@gmail.com>
2025-04-07 16:07:55 +0900

    commit_v3

--- prac_snake.py
+++ prac_snake.py
@@ -1,18 +1,31 @@
 import pygame
 pygame.init()
 
+import random
+
+food_size = 30
+def new_food():
+    fx = random.randint(0, (800 - food_size) // food_size) * food_size
+    fy = random.randint(0, (600 - food_size) // food_size) * food_size
+    return fx, fy
+
+
+
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('snake game')
 clock = pygame.time.Clock()
 
 x, y = 400, 300 # 시작할 좌표 설정
-size = 30 
+size = 30
 dx, dy = 0, 0 # 벡터 계산을 위한 변수 설정
-speed = 5 # 벡터값 갱신을 위한 값
+speed = 30 # 벡터값 갱신을 위한 값
 green = (0, 255, 0)
 white = (255, 255, 255)
 font = pygame.font.SysFont(None, 72)
+RED = ((255, 0, 0))
 
+food_x , food_y = new_food()
+    
 
 running = True
 
@@ -23,17 +36,29 @@ while running:
             running = False # 종료를 위한 조건 확인 -> 조건 달성 시 종료
             
         elif event.type == pygame.KEYDOWN: # 이벤트 분기 확인 -> 키보드가 눌릴 때
+            new_dx, new_dy = dx, dy # 입력 방향 저장
+            
             if event.key == pygame.K_LEFT: 
-                dx, dy = -speed, 0 # 방향을 설정, speed값만큼 이동 
+                new_dx, new_dy = -speed, 0 # 방향을 설정, speed값만큼 이동 
             elif event.key == pygame.K_RIGHT:
-                dx, dy = speed, 0
+                new_dx, new_dy = speed, 0
             elif event.key == pygame.K_UP:
-                dx, dy = 0, -speed
+                new_dx, new_dy = 0, -speed
             elif event.key == pygame.K_DOWN:
-                dx, dy = 0, speed
-    
+                new_dx, new_dy = 0, speed
+
+            if not (new_dx == -dx and new_dy == -dy): # 입력한 방향이 현재 방향과 정반대일때
+                dx, dy = new_dx, new_dy # 입력값 무시 
+                
     x += dx # 해당 방향으로 벡터값 갱신 
     y += dy
+    
+    
+    
+    if x == food_x and y == food_y:
+        food_x, food_y = new_food()    
+        screen.blit(text, (200, 200))
+        running = False
     
     if x < 0 or x + size > 800 or y < 0 or y + size > 600: # 각 방향의 끝에 도달했을때
         text = font.render('Game Over', True, (255, 0, 0)) # 텍스트 설정
@@ -42,11 +67,12 @@ while running:
         pygame.time.delay(2000)  # 2초 동안 보여주기
         running = False # 작동 종료 
 
-    
-    screen.fill(white)
-    pygame.draw.rect(screen, green, (x, y, size, size))
-    pygame.display.flip()
-    clock.tick(60) # FPS 60으로 설정 
-                
+    pygame.draw.rect(screen, RED, (food_x, food_y, food_size, food_size))
+
+
+    pygame.draw.rect(screen, green, (x, y, size, size)) # 도형 그리기(뱀 머리)
+    pygame.display.flip() # 그린 사각형 화면에 반환
+    clock.tick(10) # FPS 설정 
+
 
 pygame.quit()
